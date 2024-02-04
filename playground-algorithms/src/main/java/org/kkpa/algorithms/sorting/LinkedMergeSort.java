37a419bf9a4d9283ec9ab63eef1aaff220a926e4
Cristian Pena <cpena@reixcorp.com>
2024-02-04 04:59:41 -0500

    Auto Commit message - it is just for fun

---
+++ playground-algorithms/src/main/java/org/kkpa/algorithms/sorting/LinkedMergeSort.java
@@ -0,0 +1,19 @@
+package org.kkpa.algorithms.sorting;
+
+/**
+ * Try to use your custom linked list implementation.
+ */
+public class LinkedMergeSort {
+
+
+  /**
+   * Sorts a linked list in ascending order.
+   * Recursively divide the linked list into sublists containing a single node.
+   * Repeatedly merge the sublists to produce sorted sublists until one remains.
+   *
+   * @param myLinkedList Returns a sorted list.
+   */
+  public static void mergeSort(Object myLinkedList) {
+
+  }
+}
--- playground-algorithms/src/test/java/algorithms/search/BinarySearchTest.java
+++ playground-algorithms/src/test/java/algorithms/search/BinarySearchTest.java
@@ -3,6 +3,9 @@ package algorithms.search;
 import org.junit.jupiter.api.Test;
 import org.kkpa.algorithms.search.RecursiveBinarySearch;
 
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinarySearchTest {
@@ -22,11 +25,25 @@ public class BinarySearchTest {
   }
 
   @Test
-  public void testBinarySearchRecursiveFound() {
-    int[] testArray = {1, 2, 3, 4, 5};
-    int target = 3;
-    assertEquals(2, RecursiveBinarySearch.search(testArray, target));
+  public void testBinarySearchCustomCase() {
+    // Generate a sorted list of n integers from 0 to n
+    int n = 100; // Change the value of n as needed
+    int[] sortedArray = new int[n];
+    for (int i = 0; i < n; i++) {
+      sortedArray[i] = i;
+    }
+
+    // Choose a random target m where m <= n
+    int m = ThreadLocalRandom.current().nextInt(0, n + 1);
+
+    // Test the binary search with the custom case
+    int expectedIndex = Arrays.binarySearch(sortedArray, m);
+    int actualIndex = RecursiveBinarySearch.search(sortedArray, m);
+
+    // Assert that the expected and actual indices are the same
+    assertEquals(expectedIndex, actualIndex);
   }
+
 
   @Test
   public void testBinarySearchRecursiveNotFound() {
@@ -42,12 +59,6 @@ public class BinarySearchTest {
     assertEquals(2, RecursiveBinarySearch.search(testArray, target));
   }
 
-  @Test
-  public void testBinarySearchRecursiveNotFoundEdgeCase() {
-    int[] testArray = {2, 5, 8, 12, 16};
-    int target = 9;
-    assertEquals(-1, RecursiveBinarySearch.search(testArray, target));
-  }
 
 }
 
